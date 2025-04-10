a599132d5db82cc9096b9ed53df28b651c7033b3
Toma Allary <allary.toma@outlook.com>
2025-04-09 22:47:25 -0400

    Try multiple improvement over CNN
    - Tried adding Batch Norm, Dropout, Global pooling
    - Tried having more CNN layer
    - Tried bigger batch size using gradient accumulation
    - Logged more metrics in wandb
    - Added Test set evaluation on saved models

--- src/cnn_classifier.py
+++ src/cnn_classifier.py
@@ -13,33 +13,46 @@ Since n & m varies for each conversation, we shall use padding and have a fixed 
 corresponding to the longest length of (n+m). Or something close and chop longer conversations.
 """
 
+class CNNBlock(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, dropout_value=0.2, activation_function='leaky_relu'):
+        super(CNNBlock, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=1)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+        self.dropout2d = nn.Dropout2d(dropout_value)
+
+        self.activation = nn.ReLU()
+        if activation_function == 'leaky_relu':
+            self.activation = nn.LeakyReLU()
+        elif activation_function == 'gelu':
+            self.activation = nn.GELU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activation(x)
+        x = self.dropout2d(x)
+        return x
+
 class CNNClassifier(nn.Module):
 
-    def __init__(self, activation_function='relu', num_emotions=7):
+    def __init__(self, activation_function='relu', dropout_value=0.5, fc_size=32, num_blocks=4, num_emotions=7):
         super(CNNClassifier, self).__init__()
 
-        # Define constants
-        self.max_encoding_lenght = 1536
-        self.num_encoded_features = 1536 # always 1536
+        self.first_conv = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.second_conv = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
 
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(8)
-        self.pool1 = nn.MaxPool2d(2, 2)
+        self.blocks = nn.Sequential(
+            *[CNNBlock(in_channels=64, out_channels=64, kernel_size=3, dropout_value=dropout_value, activation_function=activation_function) for _ in range(num_blocks)]
+        )
 
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(16)
-        self.pool2 = nn.MaxPool2d(2, 2)
+        self.final_conv = nn.Conv2d(64, fc_size, kernel_size=3, stride=1, padding=1)
+        self.final_bn = nn.BatchNorm2d(fc_size)
+        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
 
-        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(16)
-        self.pool3 = nn.AvgPool2d(2, 2)
-
-        self.conv4 = nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(8)
-        self.pool4 = nn.AvgPool2d(2, 2)
-
-        self.dropout = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(6144, num_emotions)
+        self.fc1 = nn.Linear(fc_size, num_emotions)
 
         self.activation = nn.ReLU()
         if activation_function == 'leaky_relu':
@@ -50,12 +63,16 @@ class CNNClassifier(nn.Module):
     def forward(self, x):
         x = x.unsqueeze(1)
 
-        x = self.pool1(self.activation(self.bn1(self.conv1(x))))
-        x = self.pool2(self.activation(self.bn2(self.conv2(x))))
-        x = self.pool3(self.activation(self.bn3(self.conv3(x))))
-        x = self.pool4(self.activation(self.bn4(self.conv4(x))))
+        x = self.first_conv(x)
+        x = self.second_conv(x)
 
-        x = self.dropout(x)
+        x = self.blocks(x)
+
+        x = self.final_conv(x)
+        x = self.final_bn(x)
+        x = self.activation(x)
+        x = self.global_pool(x)
+
         x = x.reshape(x.shape[0], -1)
         x = self.fc1(x)
         return x
