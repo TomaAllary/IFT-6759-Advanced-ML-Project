a21da28cf52ca3204de1e5fe4d1c0a579d12e4d3
Toma Allary <allary.toma@outlook.com>
2025-04-10 09:27:51 -0400

    More wandb log, return to previous simple cnn

--- src/cnn_classifier.py
+++ src/cnn_classifier.py
@@ -11,8 +11,77 @@ where n is the lenght of the encoded audio and m the lenght of the encoded text.
 
 Since n & m varies for each conversation, we shall use padding and have a fixed size 
 corresponding to the longest length of (n+m). Or something close and chop longer conversations.
+
 """
 
+import torch
+from torch import optim
+from torch import nn
+from tqdm import tqdm
+
+
+
+"""
+Here inputs will have the form (n+m) X 1536
+where n is the lenght of the encoded audio and m the lenght of the encoded text.
+
+Since n & m varies for each conversation, we shall use padding and have a fixed size 
+corresponding to the longest length of (n+m). Or something close and chop longer conversations.
+"""
+
+class CNNClassifier(nn.Module):
+
+    def __init__(self, activation_function='relu', num_emotions=7):
+        super(CNNClassifier, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(16)
+        self.pool3 = nn.AvgPool2d(2, 2)
+
+        self.conv4 = nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(8)
+        self.pool4 = nn.AvgPool2d(2, 2)
+
+        self.drop2d = nn.Dropout2d(0.2)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(6144, num_emotions)
+
+        self.activation = nn.ReLU()
+        if activation_function == 'leaky_relu':
+            self.activation = nn.LeakyReLU()
+        elif activation_function == 'gelu':
+            self.activation = nn.GELU()
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+
+        x = self.pool1(self.activation(self.bn1(self.conv1(x))))
+        x = self.drop2d(x)
+        x = self.pool2(self.activation(self.bn2(self.conv2(x))))
+        x = self.drop2d(x)
+        x = self.pool3(self.activation(self.bn3(self.conv3(x))))
+        x = self.drop2d(x)
+        x = self.pool4(self.activation(self.bn4(self.conv4(x))))
+
+        x = self.dropout(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        return x
+
+
+######################################### Version 2 #########################################
+"""
+In the end, it wasn't better. 
+But this model did not overfit. Train acc and Val acc were similar (but lower than V1)
+"""
 class CNNBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size, dropout_value=0.2, activation_function='leaky_relu'):
@@ -36,10 +105,10 @@ class CNNBlock(nn.Module):
         x = self.dropout2d(x)
         return x
 
-class CNNClassifier(nn.Module):
+class CNNClassifier2(nn.Module):
 
     def __init__(self, activation_function='relu', dropout_value=0.5, fc_size=32, num_blocks=4, num_emotions=7):
-        super(CNNClassifier, self).__init__()
+        super(CNNClassifier2, self).__init__()
 
         self.first_conv = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.second_conv = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
