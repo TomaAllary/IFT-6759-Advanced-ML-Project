f9422ce26dfdc8c52b2c907aae8d4a2256022921
Toma Allary <allary.toma@outlook.com>
2025-04-15 18:26:00 -0400

    Added IEMOCAP pre-processing
    doing similar process as MELD on IEMOCAP.
    
    - Using torch dataset + local IEMOCAP
    - Using sliding window to create fix dialogue size

--- src/load.py
+++ src/load.py
@@ -3,8 +3,12 @@ import os
 import pandas as pd
 import torch.cuda
 
-from utterances_processing import MELDUtteranceProcessor
+from utterances_processing import MELDUtteranceProcessor, IEMOCAPUtteranceProcessor
 
+
+##########################################################################################
+##                                 MELD PROCESSING                                      ##
+##########################################################################################
 
 ############# PROCESS ##############
 
@@ -33,34 +37,55 @@ from utterances_processing import MELDUtteranceProcessor
 # myProcessor2.Process_pipeline()
 
 # Test set
-myProcessor3 = MELDUtteranceProcessor(
-    project_dir=os.getcwd(),
-    MELD_folder='test',
-    audio_data_folder='output_repeated_splits_test',
-    text_data_csv_filename='test_sent_emo.csv',
-    embeddings_pkl_filename='test_features_per_sentence',
-    use_cpu=True,
-)
-if myProcessor3.device == 'cuda':
-    torch.cuda.empty_cache()
-myProcessor3.Process_pipeline()
+# myProcessor3 = MELDUtteranceProcessor(
+#     project_dir=os.getcwd(),
+#     MELD_folder='test',
+#     audio_data_folder='output_repeated_splits_test',
+#     text_data_csv_filename='test_sent_emo.csv',
+#     embeddings_pkl_filename='test_features_per_sentence',
+#     use_cpu=True,
+# )
+# if myProcessor3.device == 'cuda':
+#     torch.cuda.empty_cache()
+# myProcessor3.Process_pipeline()
 
 
 
 
 ############## LOAD ################
-train_df_name = os.path.join(os.getcwd(), "MELD.Raw", "train", "train_features_per_sentence.pkl")
-dev_df_name = os.path.join(os.getcwd(), "MELD.Raw", "dev", "dev_features_per_sentence.pkl")
-test_df_name = os.path.join(os.getcwd(), "MELD.Raw", "test", "test_features_per_sentence.pkl")
+# train_df_name = os.path.join(os.getcwd(), "MELD.Raw", "train", "train_features_per_sentence.pkl")
+# dev_df_name = os.path.join(os.getcwd(), "MELD.Raw", "dev", "dev_features_per_sentence.pkl")
+# test_df_name = os.path.join(os.getcwd(), "MELD.Raw", "test", "test_features_per_sentence.pkl")
+#
+# df_features1 = pd.read_pickle(train_df_name)
+# df_features2 = pd.read_pickle(dev_df_name)
+# df_features3 = pd.read_pickle(test_df_name)
+#
+# # Check structure
+# print(f"Train features shape: {df_features1.shape}")
+# print(f"Dev features shape: {df_features2.shape}")
+# print(f"Test features shape: {df_features3.shape}")
+#
+# print(df_features1.head())
+# print(df_features1.iloc[0])
+# print(df_features1.iloc[0]["Features"].shape)
 
-df_features1 = pd.read_pickle(train_df_name)
-df_features2 = pd.read_pickle(dev_df_name)
-df_features3 = pd.read_pickle(test_df_name)
 
-# Check structure
-print(f"Train features shape: {df_features1.shape}")
-print(f"Dev features shape: {df_features2.shape}")
-print(f"Test features shape: {df_features3.shape}")
+##########################################################################################
+##                               IEMOCAP PROCESSING                                     ##
+##########################################################################################
 
-print(df_features1.head())
-print(df_features1.iloc[0])
+project_root_dir = os.path.dirname(os.getcwd())
+myProcessor = IEMOCAPUtteranceProcessor(base_dir=project_root_dir)
+
+# myCSV_df = myProcessor.create_csv() # step 1 -> create "utterances.csv"
+# # Heavy features extraction here
+# myProcessor.create_pickle(myCSV_df) # step 2 -> create "utterances_raw_features.pkl" (all feature, but no dialogueID)
+
+# dataframe = myProcessor.create_dialogue_sets(seq_length=8) # step 3 -> create a bunch of dialogue of length=8
+
+dataframe = pd.read_pickle(os.path.join(myProcessor.iemocap_dir, "utterances_features_window=8.pkl"))
+
+print(dataframe.head())
+print(dataframe.columns)
+
